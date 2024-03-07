6abe391be79365b84459c3336d5350142be2a665
Latfiansya <latfiansyadzikri@gmail.com>
2024-03-07 18:15:25 +0700

    Output dan looping

--- 7 Maret.cpp
+++ 7 Maret.cpp
@@ -14,4 +14,13 @@ void input() {
 			cout << "Array dapat mempunyai maksimal 20 elemen.\n";
 		}
 	}
+	cout << endl;
+	cout << "=====================" << endl;
+	cout << "Masukkan elemen array" << endl;
+	cout << "=====================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> a[i];
+	}
 }
